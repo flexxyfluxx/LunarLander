a7882e2e57bd47a07aeaf8d9b571792ffd097d46
flexxyfluxx <felix@vonharrach.de>
2022-05-23 00:14:59 +0200

    Terrain gen things (shrug)

--- Terrain.py
+++ Terrain.py
@@ -1,11 +1,12 @@
 from __future__ import print_function
 import ch.aplu.jgamegrid as gg
-#from time import time_ns
+from time import time
 import random as r
 
 class Terrain():
-    def __init__(self, resolution, min, max):
-        self.plot = [r.randint(min, max) for c in range(resolution)]
+    def __init__(self, resolution, min, max, seed=time()):
+        gen = r.Random(seed)
+        self.plot = [gen.randint(min, max) for c in range(resolution)]
         
     
     def print_plot(self):
@@ -19,13 +20,28 @@ class Terrain():
             self.plot = [(self.plot[c-1] + self.plot[c] + self.plot[c+1]) / 3 for c in range(len(self.plot) - 1)] \
                         + [(self.plot[-2] + self.plot[-1] + self.plot[0]) / 3]
 
+    def get_interpolated(self, steps):
+        out = []
+        for c in range(len(self.plot)-1):
+            out += [ (float(self.plot[c]) + (float(self.plot[c+1]) - float(self.plot[c])) / steps * current_step) for current_step in range(steps) ]
+        
+        # Deckel drauf!
+        out += [self.plot[-1]]
+
+        return out
+
 
 if __name__ == "__main__":
-    """
-    terra = Terrain(200, 5, 150)
+    #"""
+    terra = Terrain(10, 5, 150)
     terra.smooth_plot(4)
+    interpol = terra.get_interpolated(16)
     terra.print_plot()
+    print("\n----------\n")
+    for c in interpol:
+        print("".join(["#" for z in range(int(round(c)))]), sep="")
     """
     terra = Terrain(100)
     grid = gg.GameGrid(800, 800, 1)
+
     #"""
--- tests/interpol.py
+++ tests/interpol.py
@@ -1,14 +1,2 @@
-from __future__ import print_function
-import math as m
-from random import randint
-
-
-for c in range(100):
-    value = c
-    value = 0 if value < 30 else value
-    value = 100 if value > 70 else value
-    value = (6 * value ** 5 - 15 * value ** 4 + 10 * value ** 3) % 100
-
-    for z in range(value):
-        print("#", end = "")
-    print("")
+number = 1.2e-3
+print(number)
